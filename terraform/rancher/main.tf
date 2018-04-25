b992645944f73853de0d19aec085fa41aab107a9
Ben Fortuna <bfortuna@odecee.com.au>
2018-04-25 15:53:05 +1000

    Fixed syntax

--- terraform/rancher/main.tf
+++ terraform/rancher/main.tf
@@ -1,14 +1,15 @@
 resource "digitalocean_droplet" "rancher-master" {
-  image = "packer-1524626585"
+  image = "33755616"
   name = "rancher-master"
   region = "sgp1"
   size = "2gb"
-  ipv4_address_private = "true"
+  private_networking = true
+  monitoring = true
   volume_ids = ["${digitalocean_volume.rancher.id}"]
 }
 
 resource "digitalocean_volume" "rancher" {
   name = "rancher-store"
   region = "sgp1"
-  size = 50gb
+  size = 50
 }
