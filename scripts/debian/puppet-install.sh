2f5cbb7a7c763251586fdee67353e1d939cf02d7
Ben Fortuna <bfortuna@odecee.com.au>
2018-05-13 01:28:42 +1000

    Added packer build for puppet node

--- scripts/debian/puppet-install.sh
+++ scripts/debian/puppet-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
 sudo dpkg -i puppet5-release-xenial.deb
-sudo apt update
-sudo apt install -y puppet-agent r10k
+sudo apt-get update
+sudo apt-get install -y puppet-agent r10k hiera-eyaml
 #export PATH=/opt/puppetlabs/bin:$PATH >> /etc/environment
