176fc78b74af9274bd2df2caed8cf169b18bbc1d
Ben Fortuna <bfortuna@odecee.com.au>
2018-06-09 11:00:10 +1000

    Mount ssh keys for building terraform plans that load public keys

--- developer-env.sh
+++ developer-env.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 docker build -t developer-env . && docker run --privileged -it --rm \
   --mount type=bind,source="$(pwd)",target=/work \
+  --volume "$HOME/.ssh:/root/.ssh" \
   developer-env
