1b329b766a8d7e1c3524bc985ab1057a47be3893
Ben Fortuna <bfortuna@odecee.com.au>
2018-06-10 13:48:00 +1000

    Mount facts for environment-based puppet testing

--- developer-env.sh
+++ developer-env.sh
@@ -2,4 +2,5 @@
 docker build -t developer-env . && docker run --privileged -it --rm \
   --mount type=bind,source="$(pwd)",target=/work \
   --volume "$HOME/.ssh:/root/.ssh" \
+  --volume "$PWD/puppet/facts:/opt/puppetlabs/facter/facts.d" \
   developer-env
