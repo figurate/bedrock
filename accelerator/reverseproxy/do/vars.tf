b6f8b5d61a57273ad98c82d0779ab2092669f42d
Ben Fortuna <bfortuna@odecee.com.au>
2018-08-04 10:45:18 +1000

    Added accelerator for reverseproxy host

---
+++ accelerator/reverseproxy/do/vars.tf
@@ -0,0 +1,23 @@
+variable "do_token" {
+  description = "Digital Ocean API token"
+}
+
+variable "do_region" {
+  description = "Digital Ocean region"
+}
+
+variable "ssh_key" {
+  description = "Identifier of public key file for SSH access to droplets"
+}
+
+variable "reverseproxy_image" {
+  description = "Digital Ocean image for reverseproxy droplet"
+}
+
+variable "enabled" {
+  description = "Start/stop the reverseproxy host"
+}
+
+variable "environment" {
+  description = "Environment identifier for the reverseproxy host"
+}
