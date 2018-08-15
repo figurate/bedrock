38d59c9f5ceebed60d6883d64515f738bc6284d1
Ben Fortuna <benfortuna@gmail.com>
2018-08-15 22:56:27 +1000

    Improve build experience

--- accelerator/build.sh
+++ accelerator/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd $(dirname $0)
 docker build -t bedrock/base:do base/do
 docker build -t bedrock/terraform:aws terraform_state/aws
 docker build -t bedrock/terraform:local terraform_state/local
