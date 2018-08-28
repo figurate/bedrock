5248844f076e91a952309ad7c1ccbb1dd944728b
Ben Fortuna <benfortuna@gmail.com>
2018-08-29 00:20:18 +1000

    Added nginx image for publishing portable accelerator scripts

--- accelerator/build.sh
+++ accelerator/build.sh
@@ -7,3 +7,5 @@ docker build -t bedrock/bastion:do -f base/do/Dockerfile.do bastion/do
 docker build -t bedrock/bastion:aws -f base/do/Dockerfile.do bastion/aws
 docker build -t bedrock/user:aws -f base/do/Dockerfile.do user/aws
 docker build -t bedrock/reverseproxy:do -f base/do/Dockerfile.do reverseproxy/do
+
+docker build -t bedrock/accelerator .
