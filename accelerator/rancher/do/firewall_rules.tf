9b130439241388f0ade418e568d57fc92462e121
Ben Fortuna <benfortuna@gmail.com>
2018-09-16 14:12:31 +1000

    Enable ports for rancher server/agent communication

--- accelerator/rancher/do/firewall_rules.tf
+++ accelerator/rancher/do/firewall_rules.tf
@@ -1,185 +1,150 @@
-//resource "digitalocean_firewall" "dns" {
-//  name = "dns"
-//
-//  tags = [
-//    "${digitalocean_tag.rancher_server.id}",
-//    "${digitalocean_tag.rancher_agent.id}",
-//    "${digitalocean_tag.reverse_proxy.id}"
-//  ]
-//
-//  outbound_rule = [
-//    {
-//      protocol                = "tcp"
-//      port_range              = "53"
-//      destination_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//    {
-//      protocol                = "udp"
-//      port_range              = "53"
-//      destination_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//  ]
-//}
-//
-//resource "digitalocean_firewall" "ntp" {
-//  name = "ntp"
-//
-//  tags = [
-//    "${digitalocean_tag.rancher_server.id}",
-//    "${digitalocean_tag.rancher_agent.id}",
-//    "${digitalocean_tag.reverse_proxy.id}"
-//  ]
-//
-//  outbound_rule = [
-//    {
-//      protocol                = "udp"
-//      port_range              = "123"
-//      destination_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//  ]
-//}
-//
-//resource "digitalocean_firewall" "papertrail" {
-//  name = "papertrail"
-//
-//  tags = [
-//    "${digitalocean_tag.rancher_server.id}",
-//    "${digitalocean_tag.rancher_agent.id}",
-//    "${digitalocean_tag.reverse_proxy.id}"
-//  ]
-//
-//  outbound_rule = [
-//    {
-//      protocol                = "tcp"
-//      port_range              = "51501"
-//      destination_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//    {
-//      protocol                = "udp"
-//      port_range              = "51501"
-//      destination_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//  ]
-//}
-//
-//resource "digitalocean_firewall" "ssh" {
-//  name = "ssh"
-//
-//  tags = [
-//    "${digitalocean_tag.rancher_server.id}",
-//    "${digitalocean_tag.rancher_agent.id}",
-//    "${digitalocean_tag.reverse_proxy.id}"
-//  ]
-//
-//  inbound_rule = [
-//    {
-//      protocol           = "tcp"
-//      port_range         = "22"
-//      source_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//  ]
-//}
-//
-//resource "digitalocean_firewall" "hkp" {
-//  name = "hkp"
-//
-//  tags = [
-//    "${digitalocean_tag.rancher_server.id}",
-//    "${digitalocean_tag.rancher_agent.id}",
-//    "${digitalocean_tag.reverse_proxy.id}"
-//  ]
-//
-//  inbound_rule = [
-//    {
-//      protocol           = "tcp"
-//      port_range         = "11371"
-//      source_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//  ]
-//}
+resource "digitalocean_firewall" "default" {
+  name = "${local.uuid}"
 
-resource "digitalocean_firewall" "rancher_server" {
-  name = "rancher-server"
+  tags = [
+    "${digitalocean_tag.rancherserver.id}",
+    "${digitalocean_tag.rancheragent.id}",
+  ]
 
-  tags = ["${digitalocean_tag.rancher_server.id}"]
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "22"
+    source_tags   = ["bastion"]
+  }
+
+  outbound_rule = [
+    {
+      protocol = "tcp"
+      port_range = "80"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol = "tcp"
+      port_range = "443"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol                = "tcp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol                = "udp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol                = "udp"
+      port_range              = "123"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+}
+
+resource "digitalocean_firewall" "papertrail" {
+  name = "${local.uuid}-papertrail"
+
+  tags = [
+    "${digitalocean_tag.rancherserver.id}",
+    "${digitalocean_tag.rancheragent.id}",
+  ]
+
+  outbound_rule = [
+    {
+      protocol                = "tcp"
+      port_range              = "51501"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol                = "udp"
+      port_range              = "51501"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+}
+
+resource "digitalocean_firewall" "rancherserver" {
+  name = "${local.uuid}-rancherserver"
+
+  tags = ["${digitalocean_tag.rancherserver.id}"]
 
   inbound_rule = [
     {
       protocol           = "udp"
       port_range         = "4500"
-      source_tags        = ["${digitalocean_tag.rancher_agent.id}"]
+      source_tags        = ["${digitalocean_tag.rancheragent.id}"]
     },
     {
       protocol           = "udp"
       port_range         = "500"
-      source_tags        = ["${digitalocean_tag.rancher_agent.id}"]
-    },
-    {
-      protocol           = "tcp"
-      port_range         = "443"
-      source_tags        = ["${digitalocean_tag.reverse_proxy.id}"]
-    },
-    {
-      protocol           = "tcp"
-      port_range         = "80"
-      source_tags        = ["${digitalocean_tag.reverse_proxy.id}"]
+      source_tags        = ["${digitalocean_tag.rancheragent.id}"]
     },
     {
       protocol           = "tcp"
       port_range         = "8080"
-      source_tags        = ["${digitalocean_tag.reverse_proxy.id}", "${digitalocean_tag.rancher_agent.id}"]
+      source_tags        = ["reverseproxy", "${digitalocean_tag.rancheragent.id}"]
     },
   ]
 
   outbound_rule = [
     {
-      protocol           = "udp"
-      port_range         = "4500"
-      destination_tags        = ["${digitalocean_tag.rancher_agent.id}"]
+      protocol           = "tcp"
+      port_range         = "22"
+      destination_tags        = ["${digitalocean_tag.rancheragent.id}"]
     },
     {
-      protocol           = "udp"
-      port_range         = "500"
-      destination_tags        = ["${digitalocean_tag.rancher_agent.id}"]
+      protocol           = "tcp"
+      port_range         = "2376"
+      destination_tags        = ["${digitalocean_tag.rancheragent.id}"]
     },
     {
-      protocol                = "tcp"
-      port_range              = "443"
-      destination_addresses   = ["0.0.0.0/0", "::/0"]
-    },
-    {
-      protocol                = "tcp"
-      port_range              = "80"
-      destination_addresses   = ["0.0.0.0/0", "::/0"]
+      protocol           = "tcp"
+      port_range         = "8080"
+      destination_tags   = ["${digitalocean_tag.rancheragent.id}"]
     },
   ]
 }
 
-resource "digitalocean_firewall" "rancher_agent" {
-  name = "rancher-agent"
+resource "digitalocean_firewall" "rancheragent" {
+  name = "${local.uuid}-rancheragent"
 
-  tags = ["${digitalocean_tag.rancher_agent.id}"]
+  tags = ["${digitalocean_tag.rancheragent.id}"]
 
   inbound_rule = [
     {
+      protocol           = "tcp"
+      port_range         = "22"
+      source_tags        = ["${digitalocean_tag.rancherserver.id}"]
+    },
+    {
+      protocol           = "tcp"
+      port_range         = "2376"
+      source_tags        = ["${digitalocean_tag.rancherserver.id}"]
+    },
+    {
       protocol           = "udp"
       port_range         = "4500"
-      source_tags        = ["${digitalocean_tag.rancher_server.id}"]
+      source_tags        = ["${digitalocean_tag.rancherserver.id}"]
     },
     {
       protocol           = "udp"
       port_range         = "500"
-      source_tags        = ["${digitalocean_tag.rancher_server.id}"]
+      source_tags        = ["${digitalocean_tag.rancherserver.id}"]
     },
     {
       protocol           = "tcp"
       port_range         = "443"
-      source_tags        = ["${digitalocean_tag.reverse_proxy.id}"]
+      source_tags        = ["reverseproxy", "${digitalocean_tag.rancherserver.id}"]
     },
     {
       protocol           = "tcp"
       port_range         = "80"
-      source_tags        = ["${digitalocean_tag.reverse_proxy.id}"]
+      source_tags        = ["reverseproxy", "${digitalocean_tag.rancherserver.id}"]
+    },
+    {
+      protocol           = "tcp"
+      port_range         = "8080"
+      source_tags        = ["reverseproxy", "${digitalocean_tag.rancherserver.id}"]
     },
   ]
 
@@ -187,64 +152,17 @@ resource "digitalocean_firewall" "rancher_agent" {
     {
       protocol           = "udp"
       port_range         = "4500"
-      destination_tags        = ["${digitalocean_tag.rancher_server.id}"]
+      destination_tags        = ["${digitalocean_tag.rancherserver.id}"]
     },
     {
       protocol           = "udp"
       port_range         = "500"
-      destination_tags        = ["${digitalocean_tag.rancher_server.id}"]
-    },
-    {
-      protocol                = "tcp"
-      port_range              = "443"
-      destination_addresses   = ["0.0.0.0/0", "::/0"]
-    },
-    {
-      protocol                = "tcp"
-      port_range              = "80"
-      destination_addresses   = ["0.0.0.0/0", "::/0"]
+      destination_tags        = ["${digitalocean_tag.rancherserver.id}"]
     },
     {
       protocol                = "tcp"
       port_range              = "8080"
-      destination_tags        = ["${digitalocean_tag.rancher_server.id}"]
+      destination_tags        = ["${digitalocean_tag.rancherserver.id}"]
     },
   ]
 }
-
-//resource "digitalocean_firewall" "reverse_proxy" {
-//  name = "reverse-proxy"
-//
-//  tags = ["${digitalocean_tag.reverse_proxy.id}"]
-//
-//  inbound_rule = [
-//    {
-//      protocol           = "tcp"
-//      port_range         = "443"
-//      source_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//    {
-//      protocol           = "tcp"
-//      port_range         = "80"
-//      source_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//  ]
-//
-//  outbound_rule = [
-//    {
-//      protocol                = "tcp"
-//      port_range              = "443"
-//      destination_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//    {
-//      protocol                = "tcp"
-//      port_range              = "80"
-//      destination_addresses   = ["0.0.0.0/0", "::/0"]
-//    },
-//    {
-//      protocol                = "tcp"
-//      port_range              = "8080"
-//      destination_tags        = ["${digitalocean_tag.rancher_server.id}"]
-//    },
-//  ]
-//}
