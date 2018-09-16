c4d03f116ce6149899c0f4e1faaaa4f02f929f00
Ben Fortuna <benfortuna@gmail.com>
2018-09-16 14:13:04 +1000

    Build rancher accelerator

--- accelerator/build.sh
+++ accelerator/build.sh
@@ -7,5 +7,6 @@ docker build -t bedrock/bastion:do -f base/do/Dockerfile.do bastion/do
 docker build -t bedrock/bastion:aws -f base/do/Dockerfile.do bastion/aws
 docker build -t bedrock/user:aws -f base/do/Dockerfile.do user/aws
 docker build -t bedrock/reverseproxy:do -f base/do/Dockerfile.do reverseproxy/do
+docker build -t bedrock/rancher:do -f base/do/Dockerfile.do rancher/do
 
 docker build -t bedrock/accelerator .
