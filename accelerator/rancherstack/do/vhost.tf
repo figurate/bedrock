a7a35409cc7cedbd795c1543dc6c1f5d3ed44128
Ben Fortuna <benfortuna@gmail.com>
2018-10-14 17:20:20 +1100

    Support multiple upstream hosts

--- accelerator/rancherstack/do/vhost.tf
+++ accelerator/rancherstack/do/vhost.tf
@@ -1,11 +1,20 @@
 resource "null_resource" "vhost" {
 
+  count = "${replace(replace(var.ssl_enabled, "/false/", 1), "/true/", 0)}"
+
   triggers {
     host = "${var.reverseproxy_host}"
+    targets = "${join(",", var.target_hosts)}"
+    port = "${var.target_port}"
+    ssl_enabled = "${var.ssl_enabled}"
   }
 
   provisioner "file" {
     content = <<EOF
+upstream ${local.uuid}.internal {
+${join("\n", formatlist("    server %s:%s;", var.target_hosts, var.target_port))}
+}
+
 server {
     listen 80;
     listen [::]:80;
@@ -13,7 +22,7 @@ server {
     server_name ${var.hostname};
 
 	location / {
-        proxy_pass ${var.target_url};
+        proxy_pass http://${local.uuid}.internal;
         proxy_http_version 1.1;
 		proxy_set_header Host $host;
         proxy_set_header Upgrade $http_upgrade;
