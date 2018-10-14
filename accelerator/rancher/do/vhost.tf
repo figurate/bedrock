04fd340196c496b29f7d73c03289d89a62c2366d
Ben Fortuna <benfortuna@gmail.com>
2018-10-14 17:18:02 +1100

    Enable lets encrypt

--- accelerator/rancher/do/vhost.tf
+++ accelerator/rancher/do/vhost.tf
@@ -6,6 +6,23 @@ resource "null_resource" "vhost" {
 
   provisioner "file" {
     content = <<EOF
+upstream rancher {
+    server ${digitalocean_droplet.rancherserver.ipv4_address_private}:8080;
+}
+
+map $http_upgrade $connection_upgrade {
+    default Upgrade;
+    ''      close;
+}
+
+# Uncomment after configuring SSL
+#server {
+#    listen 80;
+#    listen [::]:80;
+#    server_name ${var.hostname};
+#    return 301 https://$server_name$request_uri;
+#}
+
 server {
     listen 80;
     listen [::]:80;
@@ -13,11 +30,14 @@ server {
     server_name ${var.hostname};
 
 	location / {
-        proxy_pass http://${digitalocean_droplet.rancherserver.ipv4_address_private}:8080;
+        proxy_pass http://rancher;
         proxy_http_version 1.1;
 		proxy_set_header Host $host;
         proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
+        proxy_set_header Connection $connection_upgrade;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Port $server_port;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }
 }
 EOF
@@ -36,7 +56,8 @@ EOF
     inline = [
       "ln -fs /etc/nginx/sites-available/${var.hostname} /etc/nginx/sites-enabled/",
       "nginx -t",
-      "nginx -s reload"
+      "nginx -s reload",
+      "certbot --non-interactive --agree-tos --nginx -d ${var.hostname}"
     ]
 
     connection {
