5e698cf1c4523fc4d3937d547708f517007fd624
Ben Fortuna <ben.fortuna@team.telstra.com>
2018-11-10 15:22:52 +1100

    Added s3 bucket accelerator

---
+++ accelerator/storage/aws/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "stack_templates" {
+  bucket = "${data.aws_caller_identity.current.account_id}-${var.bucket_name}"
+  acl    = "private"
+  versioning {enabled = true}
+}
