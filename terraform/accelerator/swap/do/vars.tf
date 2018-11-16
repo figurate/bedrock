9151cb1184ffecf5ecf3949203a0aa9ec045d567
Ben Fortuna <ben.fortuna@team.telstra.com>
2018-11-16 12:00:31 +1100

    Refactoring

---
+++ terraform/accelerator/swap/do/vars.tf
@@ -0,0 +1,12 @@
+variable "ssh_private_key" {
+  description = "Location of private key file for SSH access to droplets"
+  default = "~/.ssh/id_rsa"
+}
+
+variable "bastion_host" {
+  description = "Bastion host used to access reverse proxy"
+}
+
+variable "target_host" {
+  description = "The target host for the remote execution"
+}
