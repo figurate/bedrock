cfa7f481ec1eb93a8964c03889c9ea8bdd726dc2
Ben Fortuna <bfortuna@odecee.com.au>
2019-01-21 11:22:46 +1100

    Added object expiry

--- terraform/blueprints/storage/aws/main.tf
+++ terraform/blueprints/storage/aws/main.tf
@@ -5,8 +5,26 @@
  */
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "stack_templates" {
+resource "aws_s3_bucket" "bucket_default" {
+  count = "${var.object_expires <= 0}"
   bucket = "${data.aws_caller_identity.current.account_id}-${var.bucket_name}"
   acl    = "private"
-  versioning {enabled = true}
+  versioning {
+    enabled = "${var.version_enabled}"
+  }
+}
+
+resource "aws_s3_bucket" "bucket_with_expiration" {
+  count = "${var.object_expires > 0}"
+  bucket = "${data.aws_caller_identity.current.account_id}-${var.bucket_name}"
+  acl    = "private"
+  versioning {
+    enabled = "${var.version_enabled}"
+  }
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = "${var.object_expires}"
+    }
+  }
 }
