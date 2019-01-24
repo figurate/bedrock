c12f7bcc0db63d3d3d4088ec7f5c27a303065454
Ben Fortuna <bfortuna@odecee.com.au>
2019-01-24 14:11:35 +1100

    Added blueprint for AWS VPC

---
+++ terraform/blueprints/vpc/scripts/aws.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+docker run --privileged -it --rm \
+  --mount type=bind,source="$(pwd)",target=/work \
+  --volume "$HOME/.aws:/root/.aws" \
+  --volume "$HOME/.ssh:/root/.ssh" \
+  -e TF_BACKEND_KEY=${TF_BACKEND_KEY:-$(basename $PWD)} \
+  -e TF_APPLY_ARGS=${TF_APPLY_ARGS} \
+  -e http_proxy=${http_proxy:-} \
+  -e https_proxy=${https_proxy:-} \
+  -e no_proxy=${no_proxy:-} \
+  --net=host \
+  bedrock/vpc:aws $@
