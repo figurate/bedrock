da0edeb1a583ce6d60bb83f472c4f143dec83c2c
Ben Fortuna <bfortuna@odecee.com.au>
2019-02-09 10:41:41 +1100

    Refactoring

--- terraform/blueprints/terraform_state/scripts/aws.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker run --privileged -it --rm \
-  --mount type=bind,source="$(pwd)",target=/work \
-  --volume "$HOME/.aws:/root/.aws" \
-  -e TF_BACKEND_KEY=$(basename $PWD) \
-  bedrock/terraform:aws
