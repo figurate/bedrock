5a89f0a101083f6bf6ab55c6d8b1d5e5a39f6a8e
Ben Fortuna <bfortuna@odecee.com.au>
2019-02-25 11:52:06 +1100

    Create IAM policy to allow read/write of terraform state

--- terraform/blueprints/terraform/state/aws/main.tf
+++ terraform/blueprints/terraform/state/aws/main.tf
@@ -5,8 +5,30 @@
  */
 data "aws_caller_identity" "current" {}
 
+data "aws_iam_policy_document" "s3_terraform_access" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject"
+    ]
+    resources = ["arn:aws:s3:::${data.aws_caller_identity.current.account_id}-terraform-state/*"]
+  }
+  statement {
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = ["arn:aws:s3:::${data.aws_caller_identity.current.account_id}-terraform-state"]
+  }
+}
+
 resource "aws_s3_bucket" "tf_state" {
   bucket = "${data.aws_caller_identity.current.account_id}-terraform-state"
   acl = "private"
   versioning {enabled = true}
+}
+
+resource "aws_iam_policy" "s3_terraform_access" {
+  name = "bedrock-terraform-state"
+  policy = "${data.aws_iam_policy_document.s3_terraform_access.json}"
 }
