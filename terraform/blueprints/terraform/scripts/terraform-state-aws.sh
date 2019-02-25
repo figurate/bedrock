a3519ac0ab371fd1455270a9102806d2ac2164ea
Ben Fortuna <bfortuna@odecee.com.au>
2019-02-25 11:52:49 +1100

    Refactoring

--- terraform/blueprints/terraform/scripts/terraform-state-aws.sh
+++ terraform/blueprints/terraform/scripts/terraform-state-aws.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+BLUEPRINT="$(basename $0 .sh)"
+
 docker run --privileged -it --rm \
   --mount type=bind,source="$(pwd)",target=/work \
   --volume "$HOME/.aws:/root/.aws" \
-  -e TF_BACKEND_KEY=$(basename $PWD) \
-  bedrock/terraform-state-aws
+  -e TF_BACKEND_KEY=$BLUEPRINT/$(basename $PWD) \
+  -e AWS_PROFILE=${AWS_PROFILE-default} \
+  -e TF_VAR_region=${AWS_DEFAULT_REGION} \
+  bedrock/$BLUEPRINT $@
