600b8e98bfe0f5cc65628b24892369ae7c9573d2
Ben Fortuna <bfortuna@odecee.com.au>
2019-02-26 00:46:34 +1100

    Provision roles for nginx administration and cloudformation stack creation

---
+++ terraform/blueprints/nginx/roles/main.tf
@@ -0,0 +1,67 @@
+/**
+ * # AWS NGINX role configuration
+ *
+ * Purpose: Provision IAM roles in AWS.
+ *
+ * Rationale: Bedrock blueprints use IAM roles to restrict the privileges of the provisioner.
+ *
+ * This script will create roles that has the following privileges:
+ *
+ * * Access for managing NGINX hosts specific to this blueprint
+ * * Access to read/write Terraform state associated with the account
+ */
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      type = "AWS"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "iam_passrole_policy" {
+  statement {
+    actions = ["iam:PassRole"]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/bedrock-nginx-cloudformation"]
+  }
+}
+
+data "aws_iam_policy_document" "cloudformation_create_policy" {
+  statement {
+    actions = ["cloudformation:Create*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "nginx_admin" {
+  name = "bedrock-nginx-admin"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+  role = "${aws_iam_role.nginx_admin.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
+  role = "${aws_iam_role.nginx_admin.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudformation_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSCloudFormationReadOnlyAccess"
+  role = "${aws_iam_role.nginx_admin.name}"
+}
+
+resource "aws_iam_role_policy" "iam_passrole" {
+  policy = "${data.aws_iam_policy_document.iam_passrole_policy.json}"
+  role = "${aws_iam_role.nginx_admin.id}"
+}
+
+resource "aws_iam_role_policy" "cloudformation_create" {
+  policy = "${data.aws_iam_policy_document.cloudformation_create_policy.json}"
+  role = "${aws_iam_role.nginx_admin.id}"
+}
