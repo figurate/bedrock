f0758b9178322a7f5da2fbb5abcc9f0000e8aa24
Ben Fortuna <bfortuna@odecee.com.au>
2019-02-27 00:37:09 +1100

    Support dynamodb table creation and import from s3 bucket

---
+++ terraform/blueprints/dynamodb/table/main.tf
@@ -0,0 +1,7 @@
+resource "aws_dynamodb_table" "table" {
+  name = "${var.db_table}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "${var.db_hash_key}"
+  range_key = "${var.range_key}"
+  attribute = "${var.db_attributes}"
+}
