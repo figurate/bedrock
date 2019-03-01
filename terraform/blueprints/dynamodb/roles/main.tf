7b8a05b7d61855879f67b867793b22f4951c333e
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-02 10:48:16 +1100

    Create roles required for dynamodb administration

--- terraform/blueprints/dynamodb/roles/main.tf
+++ terraform/blueprints/dynamodb/roles/main.tf
@@ -22,35 +22,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-data "aws_iam_policy_document" "iam_passrole_policy" {
-  statement {
-    actions = ["iam:PassRole"]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/bedrock-dynamodb-import-role"]
-  }
-}
-
-data "aws_iam_policy_document" "iam_access" {
-  statement {
-    actions = [
-      "iam:PutRolePolicy",
-      "iam:GetRolePolicy",
-    ]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/bedrock-dynamodb-import-role"]
-  }
-  statement {
-    actions = [
-      "lambda:*",
-    ]
-    resources = ["arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:DynamoDBImportCsv"]
-  }
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-    ]
-    resources = ["*"]
-  }
-}
-
 resource "aws_iam_role" "dynamodb_admin" {
   name = "bedrock-dynamodb-admin"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
@@ -61,17 +32,17 @@ resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {
   role = "${aws_iam_role.dynamodb_admin.name}"
 }
 
-resource "aws_iam_role_policy" "iam_access" {
-  role = "${aws_iam_role.dynamodb_admin.id}"
-  policy = "${data.aws_iam_policy_document.iam_access.json}"
-}
-
-resource "aws_iam_role_policy" "iam_passrole" {
-  policy = "${data.aws_iam_policy_document.iam_passrole_policy.json}"
-  role = "${aws_iam_role.dynamodb_admin.id}"
-}
-
 resource "aws_iam_role_policy_attachment" "s3_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role = "${aws_iam_role.dynamodb_admin.id}"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_full_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaFullAccess"
+  role = "${aws_iam_role.dynamodb_admin.id}"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_passrole" {
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/bedrock-iam-passrole"
   role = "${aws_iam_role.dynamodb_admin.id}"
 }
