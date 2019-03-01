b41df0e1eb3ce7fee2a96b95997b85995e5cae47
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-02 09:25:10 +1100

    Create common iam policies for roles

---
+++ terraform/blueprints/iam/policies/main.tf
@@ -0,0 +1,58 @@
+/**
+ * # AWS IAM policies
+ *
+ * Purpose: Provision IAM policies in AWS.
+ *
+ * Rationale: Bedrock blueprints use IAM roles to restrict the privileges of the provisioner.
+ *
+ * This script will create policies for common activities that may be attached to roles.
+ */
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "iam_passrole_policy" {
+  statement {
+    actions = ["iam:PassRole"]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/bedrock-*"]
+  }
+}
+
+data "aws_iam_policy_document" "cloudformation_create_policy" {
+  statement {
+    actions = [
+      "cloudformation:Create*",
+      "cloudformation:Update*",
+      "cloudformation:Delete*",
+    ]
+    resources = ["arn:aws:cloudformation:${var.region}:${data.aws_caller_identity.current.account_id}:stack/*"]
+  }
+}
+
+data "aws_iam_policy_document" "ec2_subnet_fullaccess" {
+  statement {
+    actions = [
+      "ec2:CreateSubnet",
+      "ec2:DeleteSubnet",
+      "ec2:DisassociateSubnetCidrBlock",
+      "ec2:ModifySubnetAttribute",
+      "ec2:DescribeSubnets",
+      "ec2:AssociateSubnetCidrBlock",
+      "ec2:CreateDefaultSubnet",
+    ]
+    resources = ["arn:aws:cloudformation:${var.region}:${data.aws_caller_identity.current.account_id}:*"]
+  }
+}
+
+resource "aws_iam_policy" "ec2_subnet_fullaccess" {
+  name = "bedrock-ec2-subnet-fullaccess"
+  policy = "${data.aws_iam_policy_document.ec2_subnet_fullaccess.json}"
+}
+
+resource "aws_iam_policy" "iam_passrole" {
+  name = "bedrock-iam-passrole"
+  policy = "${data.aws_iam_policy_document.iam_passrole_policy.json}"
+}
+
+resource "aws_iam_policy" "cloudformation_create" {
+  name = "bedrock-cloudformation-create"
+  policy = "${data.aws_iam_policy_document.cloudformation_create_policy.json}"
+}
