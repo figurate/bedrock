04f885219d0e11c13e73f0375d2d5cf7cb48ec82
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-02 10:47:30 +1100

    Create common iam policies for roles

--- terraform/blueprints/iam/policies/main.tf
+++ terraform/blueprints/iam/policies/main.tf
@@ -45,19 +45,34 @@ data "aws_iam_policy_document" "ec2_subnet_fullaccess" {
 data "aws_iam_policy_document" "ec2_instance_profile_fullaccess" {
   statement {
     actions = [
-      "iam:Create*",
-      "iam:AttachRolePolicy",
-      "iam:DetachRolePolicy",
-      "iam:Delete*",
+      "iam:GetRole",
     ]
-    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/bedrock-*"]
+    resources = ["*"]
   }
   statement {
     actions = [
+      "iam:CreateInstanceProfile",
+      "iam:DeleteInstanceProfile",
+      "iam:GetInstanceProfile",
       "iam:AddRoleToInstanceProfile",
       "iam:RemoveRoleFromInstanceProfile",
     ]
     resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/bedrock-*"]
+  }
+}
+
+data "aws_iam_policy_document" "lambda_dynamodb_fullaccess" {
+  statement {
+    actions = [
+      "dynamodb:List*",
+      "dynamodb:Describe*",
+      "dynamodb:Get*",
+      "dynamodb:PutItem",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+    ]
+    resources = ["*"]
   }
 }
 
@@ -69,6 +84,11 @@ resource "aws_iam_policy" "ec2_subnet_fullaccess" {
 resource "aws_iam_policy" "ec2_instance_profile_fullaccess" {
   name = "bedrock-ec2-instance-profile-fullaccess"
   policy = "${data.aws_iam_policy_document.ec2_instance_profile_fullaccess.json}"
+}
+
+resource "aws_iam_policy" "lambda_dynamodb_fullaccess" {
+  name = "bedrock-lambda-dynamodb-fullaccess"
+  policy = "${data.aws_iam_policy_document.lambda_dynamodb_fullaccess.json}"
 }
 
 resource "aws_iam_policy" "iam_passrole" {
