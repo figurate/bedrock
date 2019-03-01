4092f768ee3f9d5534f5b60dd10b6166d90fa4b7
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-02 10:04:41 +1100

    Create roles required for nginx administration

--- terraform/blueprints/nginx/roles/cloudformation.tf
+++ terraform/blueprints/nginx/roles/cloudformation.tf
@@ -8,27 +8,6 @@ data "aws_iam_policy_document" "cloudformation_assume_role_policy" {
   }
 }
 
-data "aws_iam_policy_document" "cloudformation_iam_passrole_policy" {
-  statement {
-    actions = ["iam:PassRole"]
-    resources = ["*"]
-  }
-}
-
-data "aws_iam_policy_document" "iam_create_policy" {
-  statement {
-    actions = [
-      "iam:Create*",
-      "iam:AttachRolePolicy",
-      "iam:DetachRolePolicy",
-      "iam:Delete*",
-      "iam:AddRoleToInstanceProfile",
-      "iam:RemoveRoleFromInstanceProfile",
-    ]
-    resources = ["*"]
-  }
-}
-
 resource "aws_iam_role" "nginx_cloudformation" {
   name = "bedrock-nginx-cloudformation"
   assume_role_policy = "${data.aws_iam_policy_document.cloudformation_assume_role_policy.json}"
@@ -39,12 +18,12 @@ resource "aws_iam_role_policy_attachment" "ec2_full_access" {
   role = "${aws_iam_role.nginx_cloudformation.name}"
 }
 
-resource "aws_iam_role_policy" "cloudformation_iam_passrole" {
-  policy = "${data.aws_iam_policy_document.cloudformation_iam_passrole_policy.json}"
+resource "aws_iam_role_policy_attachment" "cloudformation_iam_passrole" {
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/bedrock-iam-passrole"
   role = "${aws_iam_role.nginx_cloudformation.id}"
 }
 
-resource "aws_iam_role_policy" "iam_create" {
-  policy = "${data.aws_iam_policy_document.iam_create_policy.json}"
+resource "aws_iam_role_policy_attachment" "ec2_instance_profile_fullaccess" {
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/bedrock-ec2-instance-profile-fullaccess"
   role = "${aws_iam_role.nginx_cloudformation.id}"
 }
