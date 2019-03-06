cc98b278f2aa38b90d4f88869cfef4ba15e33c22
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-06 11:17:04 +1100

    Support for bastion host on amazon linux

--- terraform/blueprints/bastion/aws/main.tf
+++ terraform/blueprints/bastion/aws/main.tf
@@ -5,6 +5,21 @@
  */
 data "aws_caller_identity" "current" {}
 
+data "aws_vpc" "default" {
+  //  default = true
+  filter {
+    name = "tag:Environment"
+    values = ["dev"]
+  }
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  tags {
+    Name = "public_subnet_*"
+  }
+}
+
 data "aws_ami" "bastion_image" {
   filter {
     name = "name"
@@ -18,24 +33,56 @@ data "aws_ami" "bastion_image" {
   owners = ["${replace(var.image_owner, "/\\A\\z/", data.aws_caller_identity.current.account_id)}"]
 }
 
-resource "aws_iam_user_ssh_key" "aws_ssh_key" {
-  encoding = "SSH"
-  public_key = "${file(var.ssh_key)}"
-  username = "${var.bastion_user}"
+data "template_file" "userdata" {
+  template = "${file(format("%s/%s.yml", var.userdata_path, var.image_os))}"
+  vars {
+    AuthorizedUserName = "${var.bastion_user}"
+    AuthorizedUserSSHKey = "${file(var.ssh_key)}"
+    ShutdownDelay = "${var.shutdown_delay}"
+  }
 }
 
-resource "aws_security_group" "bastion_sg" {
+data "aws_iam_role" "instance" {
+  name = "bedrock-bastion-instance"
+}
+
+resource "aws_security_group" "bastion" {
   name = "bastion-sg"
   ingress {
-    from_port = 22
     protocol = "TCP"
+    from_port = 22
     to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    protocol = "TCP"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol = "TCP"
+    from_port = 443
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bedrock-bastion-instance"
+  role = "${data.aws_iam_role.instance.name}"
 }
 
 resource "aws_instance" "bastion" {
   count = "${replace(replace(var.enabled, "/false/", 0), "/true/", 1)}"
   ami = "${data.aws_ami.bastion_image.id}"
   instance_type = "${var.instance_type}"
-  security_groups = ["${aws_security_group.bastion_sg.id}"]
+  security_groups = ["${aws_security_group.bastion.name}"]
+  user_data = "${data.template_file.userdata.rendered}"
+  iam_instance_profile = "${aws_iam_instance_profile.bastion.name}"
+  instance_initiated_shutdown_behavior = "terminate"
+  tags {
+    Name = "bastion"
+  }
+  vpc_security_group_ids = ["${data.aws_subnet_ids.public.ids}"]
 }
--- terraform/blueprints/bastion/aws/vars.tf
+++ terraform/blueprints/bastion/aws/vars.tf
@@ -1,9 +1,15 @@
+variable "userdata_path" {
+  description = "The root path to userdata templates"
+  default = "userdata"
+}
+
 variable "bastion_user" {
   description = "Username for bastion SSH user"
 }
 
 variable "ssh_key" {
   description = "Location of public key file for SSH access to droplets"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "image_name" {
@@ -16,11 +22,26 @@ variable "image_owner" {
   default = "137112412989"
 }
 
+variable "image_os" {
+  description = <<EOF
+The operating system installed on the selected AMI. Valid values are:
+
+  * al2     = Amazon Linux 2
+  * ubuntu  = Ubuntu
+EOF
+  default = "al2"
+}
+
 variable "instance_type" {
   description = "AWS instance type for bastion"
-  default = "t3.micro"
+  default = "t3.nano"
 }
 
 variable "enabled" {
   description = "Start/stop the bastion host"
+}
+
+variable "shutdown_delay" {
+  description = "Number of minutes before the host will automatically shutdown"
+  default = 60
 }
