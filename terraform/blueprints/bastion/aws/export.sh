1317e93be1821ca5d20b8670eefa3898c84d1bd2
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-06 11:17:04 +1100

    Support for bastion host on amazon linux

---
+++ terraform/blueprints/bastion/aws/export.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Generate terraform backend config
+sh /bootstrap/backend_tf.sh > /bootstrap/backend.tf
+
+# export terraform files to current directory
+cp /bootstrap/*.tf ./
+cp -R /bootstrap/userdata ./
--- terraform/blueprints/bastion/aws/provider.tf
+++ terraform/blueprints/bastion/aws/provider.tf
@@ -1,7 +1,14 @@
 provider "aws" {
   region = "${var.region}"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.assume_role_account}:role/bedrock-bastion-admin"
+  }
 }
 
 variable "region" {
   description = "AWS default region"
+}
+
+variable "assume_role_account" {
+  description = "AWS account ID for the role to assume into"
 }
