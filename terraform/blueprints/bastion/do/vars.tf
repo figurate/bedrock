11e73e551bef4b2fff3e65b74ff569419cc53040
Ben Fortuna <benfortuna@gmail.com>
2019-03-14 18:37:10 +1100

    Refactoring

--- terraform/blueprints/bastion/do/vars.tf
+++ terraform/blueprints/bastion/do/vars.tf
@@ -1,3 +1,12 @@
+variable "userdata_path" {
+  description = "The root path to userdata templates"
+  default = "userdata"
+}
+
+variable "bastion_user" {
+  description = "Username for bastion SSH user"
+}
+
 variable "ssh_key" {
   description = "Identifier of public key file for SSH access to droplets"
 }
@@ -12,8 +21,22 @@ variable "bastion_image" {
   default = "ubuntu-18-04-x64"
 }
 
+variable "image_os" {
+  description = <<EOF
+The operating system installed on the selected droplet. Valid values are:
+
+  * ubuntu  = Ubuntu
+EOF
+  default = "ubuntu"
+}
+
 variable "enabled" {
   description = "Start/stop the bastion host"
+}
+
+variable "shutdown_delay" {
+  description = "Number of minutes before the host will automatically shutdown"
+  default = 60
 }
 
 locals {
