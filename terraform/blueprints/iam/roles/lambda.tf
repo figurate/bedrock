ce4cc0d96e0ee587a9df205af80c22d55553b7ef
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-21 09:06:50 +1100

    Provide roles for IAM management

---
+++ terraform/blueprints/iam/roles/lambda.tf
@@ -0,0 +1,24 @@
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "iam_keyrotation" {
+  name = "bedrock-iam-keyrotation-role"
+  assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_fullaccess" {
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+  role = "${aws_iam_role.iam_keyrotation.id}"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/bedrock-cloudwatch-logs"
+  role = "${aws_iam_role.iam_keyrotation.id}"
+}
