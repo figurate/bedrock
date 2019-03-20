6184f9f49881d21cc4c4e1ae4c261fad6b734168
Ben Fortuna <bfortuna@odecee.com.au>
2019-03-21 09:05:03 +1100

    Include power users in key rotation group by default

--- terraform/blueprints/iam/keyrotation/main.tf
+++ terraform/blueprints/iam/keyrotation/main.tf
@@ -20,9 +20,18 @@ resource "aws_lambda_function" "iam_keyrotation" {
   role = "${data.aws_iam_role.iam_keyrotation.arn}"
   runtime = "python3.6"
   source_code_hash = "${data.archive_file.iam_keyrotation.output_base64sha256}"
+  environment {
+    variables {
+      KeyMaxAge = "${var.key_max_age}"
+    }
+  }
 }
 
 resource "aws_cloudwatch_log_group" "iam_keyrotation" {
   name = "/aws/lambda/${aws_lambda_function.iam_keyrotation.function_name}"
   retention_in_days = 30
+}
+
+resource "aws_iam_group" "iam_keyrotation" {
+  name = "iam-keyrotation"
 }
