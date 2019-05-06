8aa64986efa41f230d12836a522cd83e9b336df8
Ben Fortuna <bfortuna@odecee.com.au>
2019-05-07 09:34:52 +1000

    Support for DynamoDB management including table creation, import and put operations

--- terraform/blueprints/dynamodb/import/vars.tf
+++ terraform/blueprints/dynamodb/import/vars.tf
@@ -3,8 +3,8 @@ variable "lambda_path" {
   default = "lambda"
 }
 
-variable "bucket_name" {
-  description = "The suffix of the S3 bucket used to import data"
+variable "function_name" {
+  description = "A unique name used to reference the function"
 }
 
 variable "data_types" {
@@ -15,6 +15,17 @@ variable "data_types" {
 
 variable "table_name" {
   description = "The name of the DynamoDB table to import into"
+}
+
+variable "item_template" {
+  description = "A template map used to initialise each item"
+  type = "map"
+  default = {}
+}
+
+variable "auto_generate_key" {
+  description = "Indicates whether the import function should automatically generate a unique partition key"
+  default = true
 }
 
 variable "import_timeout" {
--- terraform/blueprints/dynamodb/table/main.tf
+++ terraform/blueprints/dynamodb/table/main.tf
@@ -1,3 +1,8 @@
+/**
+ * # DynamoDB Create Table
+ *
+ * Support creation of DynamoDB tables.
+ */
 resource "aws_dynamodb_table" "table" {
   name = "${var.db_table}"
   billing_mode = "PAY_PER_REQUEST"
