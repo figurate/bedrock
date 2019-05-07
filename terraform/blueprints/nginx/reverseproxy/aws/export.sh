c02b118b80eca66aa13785c607f34abaf5d4b1ac
Ben Fortuna <bfortuna@odecee.com.au>
2019-05-07 14:47:28 +1000

    Support public and private routing

--- terraform/blueprints/nginx/reverseproxy/aws/export.sh
+++ terraform/blueprints/nginx/reverseproxy/aws/export.sh
@@ -6,3 +6,4 @@ sh /bootstrap/backend_tf.sh > /bootstrap/backend.tf
 # export terraform files to current directory
 cp /bootstrap/*.tf ./
 cp -R /bootstrap/cloudformation ./
+cp -R /bootstrap/userdata ./
