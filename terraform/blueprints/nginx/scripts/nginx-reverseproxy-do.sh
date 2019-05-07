6061812bff89a48776d3bfb8d1e17626e91fa258
Ben Fortuna <bfortuna@odecee.com.au>
2019-05-07 14:50:56 +1000

    Refactoring

--- terraform/blueprints/nginx/scripts/nginx-reverseproxy-do.sh
+++ terraform/blueprints/nginx/scripts/nginx-reverseproxy-do.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-docker run --privileged -it --rm \
-  --mount type=bind,source="$(pwd)",target=/work \
-  --volume "$HOME/.aws:/root/.aws" \
-  --volume "$HOME/.ssh:/root/.ssh" \
-  -e TF_BACKEND_KEY=${TF_BACKEND_KEY:-$(basename $PWD)} \
-  bedrock/reverseproxy-host-do
+BEDROCK_BLUEPRINT="$(basename $0 .sh)"
+
+. $(dirname $0)/../../../scripts/blueprint-run.sh $@
--- terraform/blueprints/nginx/scripts/nginx-vhost.sh
+++ terraform/blueprints/nginx/scripts/nginx-vhost.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-docker run --privileged -it --rm \
-  --mount type=bind,source="$(pwd)",target=/work \
-  --volume "$HOME/.aws:/root/.aws" \
-  --volume "$HOME/.ssh:/root/.ssh" \
-  -e TF_BACKEND_KEY=${TF_BACKEND_KEY:-$(basename $PWD)} \
-  -e TF_STACK=${TF_STACK} \
-  -e TF_ENVIRONMENT=${TF_ENVIRONMENT} \
-  -e TF_ENV_SUFFIX=${TF_ENV_SUFFIX} \
-  bedrock/reverseproxy-vhost $@
+BEDROCK_BLUEPRINT="$(basename $0 .sh)"
+
+. $(dirname $0)/../../../scripts/blueprint-run.sh $@
