03f9766b4b52d1de1edf73e7ea4334369e8d4ba4
Ben Fortuna <bfortuna@odecee.com.au>
2019-05-16 09:45:52 +1000

    Refactoring

--- developer-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker build -t developer-env . && docker run --privileged -it --rm \
-  --mount type=bind,source="$(pwd)",target=/work \
-  --volume "$HOME/.ssh:/root/.ssh" \
-  --volume "$PWD/puppet/facts:/opt/puppetlabs/facter/facts.d" \
-  developer-env
--- terraform/modules/tf_module_storage/main.tf
+++
@@ -1,7 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "tf_modules" {
-  bucket = "${data.aws_caller_identity.current.account_id}-terraform-modules"
-  acl = "private"
-  versioning {enabled = true}
-}
