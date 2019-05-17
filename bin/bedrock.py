f51b7480f5969c890e929b8697f5207d76f51c83
Ben Fortuna <bfortuna@odecee.com.au>
2019-05-17 17:29:14 +1000

    Support additional overrides for blueprint inputs

--- bin/bedrock.py
+++ bin/bedrock.py
@@ -31,7 +31,7 @@ def parse_manifest(file):
     return yaml.load(file, Loader=Loader)
 
 
-def apply_blueprint(name, key, config, action, extra_volumes):
+def apply_blueprint(name, key, config, action, extra_volumes, extra_config):
     print(f'Apply blueprint: {name}/{key} [{action}]')
 
     init_path(f'{name}/{key}')
@@ -56,6 +56,11 @@ def apply_blueprint(name, key, config, action, extra_volumes):
             else:
                 environment.append(f'TF_VAR_{item}={config[item]}')
 
+    if extra_config:
+        for cnf in extra_config:
+            cargs = cnf.split('=')
+            environment.append(f'TF_VAR_{cargs[0]}={cargs[1]}')
+
     volumes = {
         expanduser(f'~/.bedrock/{name}/{key}'): {
             'bind': '/work',
@@ -63,12 +68,13 @@ def apply_blueprint(name, key, config, action, extra_volumes):
         }
     }
 
-    for volume in extra_volumes:
-        vargs = volume.split(':')
-        volumes[vargs[0]] = {
-            'bind': vargs[1],
-            'mode': 'ro'
-        }
+    if extra_volumes:
+        for volume in extra_volumes:
+            vargs = volume.split(':')
+            volumes[vargs[0]] = {
+                'bind': vargs[1],
+                'mode': 'ro'
+            }
 
     container = client.containers.run(f"bedrock/{name}", action, privileged=True, network_mode='host',
                           remove=True, environment=environment, volumes=volumes, tty=True, detach=True)
@@ -82,6 +88,8 @@ parser.add_argument('-m', '--manifest', metavar='<manifest_path>', default='mani
                     help='location of manifest file (default: %(default)s)')
 parser.add_argument('-v', '--volumes', metavar='<path:volume>', nargs='+',
                     help='additional volumes mounted to support blueprints')
+parser.add_argument('-c', '--config', metavar='<key=value>', nargs='+',
+                    help='additional configuration to support blueprints')
 parser.add_argument('action', metavar='<command>', choices=['init', 'apply', 'plan', 'destroy'],
                     help='manifest action (possible values: %(choices)s)')
 
@@ -93,4 +101,4 @@ for constellation in manifest['constellations']:
     blueprint_key = constellation
     for blueprint in manifest['constellations'][constellation]:
         apply_blueprint(blueprint, blueprint_key, manifest['constellations'][constellation][blueprint],
-                        args.action, args.volumes)
+                        args.action, args.volumes, args.config)
