4f6bd4df175bc52a42445217af79454017cada7e
Ben Fortuna <bfortuna@odecee.com.au>
2019-06-01 09:13:57 +1000

    Use environment variables to set common configuration

--- blueprints/base/environment/terraform_tfvars.sh
+++ blueprints/base/environment/terraform_tfvars.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 ENVIRONMENT=${1:-$TF_ENVIRONMENT}
 ENV_SUFFIX=${2:-$TF_ENV_SUFFIX}
-AWS_ACCOUNT=$(aws sts get-caller-identity | jq -r '.Account')
 cat <<EOF
 environment="$ENVIRONMENT"
 env_suffix="$ENV_SUFFIX"
-assume_role_account="$AWS_ACCOUNT"
 EOF
