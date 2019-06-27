5bff896002c8674d59bdce5fb6859086178990f5
Ben Fortuna <bfortuna@odecee.com.au>
2019-06-27 11:56:06 +1000

    Use more specific var names to avoid conflict with other blueprints

--- blueprints/bastion/aws/main.tf
+++ blueprints/bastion/aws/main.tf
@@ -6,34 +6,36 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_vpc" "tenant" {
-  default = "${var.vpc_default}"
-  tags    = "${var.vpc_tags}"
+  default = var.vpc_default
+  tags    = var.vpc_tags
 }
 
 data "aws_ami" "bastion_image" {
   filter {
-    name   = "name"
-    values = ["${var.image_name}"]
+    name = "name"
+    values = [
+    var.image_name]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name   = "state"
+    values = ["available"]
   }
 
   most_recent = true
-  owners      = ["${replace(var.image_owner, "/\\A\\z/", data.aws_caller_identity.current.account_id)}"]
+  owners = [
+  replace(var.image_owner, "/\\A\\z/", data.aws_caller_identity.current.account_id)]
 }
 
 data "template_file" "userdata" {
-  template = "${file(format("%s/%s.yml", var.userdata_path, var.image_os))}"
+  template = file(format("%s/%s.yml", var.userdata_path, var.image_os))
 
   vars = {
     AuthorizedUserName = var.bastion_user
 
     //    AuthorizedUserSSHKey = "${replace(var.ssh_key, "/\\A\\z/", file(var.ssh_key_file))}"
-    AuthorizedUserSSHKey = "${replace(var.ssh_key, "/\\A\\z/", "")}"
-    ShutdownDelay        = "${var.shutdown_delay}"
+    AuthorizedUserSSHKey = replace(var.ssh_key, "/\\A\\z/", "")
+    ShutdownDelay        = var.shutdown_delay
   }
 }
 
@@ -66,10 +68,11 @@ resource "aws_security_group" "bastion" {
   }
 
   egress {
-    protocol    = "TCP"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["${data.aws_vpc.tenant.cidr_block}"]
+    protocol  = "TCP"
+    from_port = 22
+    to_port   = 22
+    cidr_blocks = [
+    data.aws_vpc.tenant.cidr_block]
   }
 
   tags = {
@@ -79,16 +82,17 @@ resource "aws_security_group" "bastion" {
 
 resource "aws_iam_instance_profile" "bastion" {
   name = "bedrock-bastion-instance"
-  role = "${data.aws_iam_role.instance.name}"
+  role = data.aws_iam_role.instance.name
 }
 
 resource "aws_instance" "bastion" {
-  count                                = "${replace(replace(var.enabled, "/false/", 0), "/true/", 1)}"
-  ami                                  = "${data.aws_ami.bastion_image.id}"
-  instance_type                        = "${var.instance_type}"
-  security_groups                      = ["${aws_security_group.bastion.name}"]
-  user_data                            = "${data.template_file.userdata.rendered}"
-  iam_instance_profile                 = "${aws_iam_instance_profile.bastion.name}"
+  count         = replace(replace(var.enabled, "/false/", 0), "/true/", 1)
+  ami           = data.aws_ami.bastion_image.id
+  instance_type = var.instance_type
+  security_groups = [
+  aws_security_group.bastion.name]
+  user_data                            = data.template_file.userdata.rendered
+  iam_instance_profile                 = aws_iam_instance_profile.bastion.name
   instance_initiated_shutdown_behavior = "terminate"
 
   tags = {
@@ -101,9 +105,10 @@ data "aws_route53_zone" "primary" {
 }
 
 resource "aws_route53_record" "bastion" {
-  zone_id = "${data.aws_route53_zone.primary.zone_id}"
-  name    = "${var.fqdn}"
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = var.bastion_fqdn
   type    = "CNAME"
   ttl     = var.record_ttl
-  records = [aws_instance.bastion[0].public_dns]
+  records = [
+  aws_instance.bastion[0].public_dns]
 }
