22fd80b505879c0bf4fcce8d13c1c1f0c6885226
Ben Fortuna <bfortuna@odecee.com.au>
2019-06-27 11:33:52 +1000

    Simplified terraform backend config

--- blueprints/base/stack/terraform_tfvars.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-STACK=${1:-$TF_STACK}
-ENVIRONMENT=${2:-$TF_ENVIRONMENT}
-ENV_SUFFIX=${3:-$TF_ENV_SUFFIX}
-cat <<EOF
-environment="$ENVIRONMENT"
-env_suffix="$ENV_SUFFIX"
-stack_name="$STACK"
-EOF
