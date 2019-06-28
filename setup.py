64a8e73bac1554fda7d5cea5581ec6b211428e7f
Ben Fortuna <bfortuna@odecee.com.au>
2019-06-28 17:18:02 +1000

    Package for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='brock',
-    version='1.0',
+    version='1.0.1',
     author='Ben Fortuna',
     author_email='fortuna@micronode.com',
     description='A collection of Terraform-based blueprints',
@@ -13,6 +13,11 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/micronode/bedrock',
     packages=find_packages(),
+    install_requires=[
+        'PyYAML>=5.1',
+        'docker>=3.7.2',
+    ],
+    python_requires='>=3.7',
     entry_points={
         'console_scripts': [
             'bmt = bedrock.manifest:main',
