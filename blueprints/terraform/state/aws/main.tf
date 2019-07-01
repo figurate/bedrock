960da4e1712b055e9c1e1c10b95c99885510cc69
Ben Fortuna <bfortuna@odecee.com.au>
2019-07-01 11:16:40 +1000

    Use environment variables to set common configuration

--- blueprints/terraform/state/aws/main.tf
+++ blueprints/terraform/state/aws/main.tf
@@ -7,11 +7,11 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "tf_state" {
   bucket = "${data.aws_caller_identity.current.account_id}-terraform-state"
-  acl = "private"
-  versioning {enabled = true}
+  acl    = "private"
+  versioning { enabled = true }
 }
 
 resource "aws_s3_bucket_public_access_block" "tf_state" {
-  bucket = "${aws_s3_bucket.tf_state.id}"
+  bucket                  = aws_s3_bucket.tf_state.id
   restrict_public_buckets = "true"
 }
