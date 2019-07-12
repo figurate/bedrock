01cc2bbeb112b1bf11bf0d81fb46ab10be69f3ef
Ben Fortuna <bfortuna@odecee.com.au>
2019-07-12 11:49:57 +1000

    Formatting

--- blueprints/nginx/reverseproxy/do/main.tf
+++ blueprints/nginx/reverseproxy/do/main.tf
@@ -6,12 +6,12 @@
 data "template_file" "userdata" {
   template = file(format("%s/%s.yml", var.userdata_path, var.image_os))
   vars = {
-    NginxAmplifyKey = var.amplify_key
-    NginxHostname = "${var.environment}-reverseproxy"
-    AuthorizedUserName = "${var.reverseproxy_user}"
-    AuthorizedUserSSHKey = "${file(var.ssh_key)}"
-    PapertrailHost = "${var.papertrail_host}"
-    PapertrailPort = "${var.papertrail_port}"
+    NginxAmplifyKey      = var.amplify_key
+    NginxHostname        = "${var.environment}-reverseproxy"
+    AuthorizedUserName   = var.reverseproxy_user
+    AuthorizedUserSSHKey = file(var.ssh_key)
+    PapertrailHost       = var.papertrail_host
+    PapertrailPort       = var.papertrail_port
   }
 }
 
@@ -20,19 +20,19 @@ resource "digitalocean_tag" "reverseproxy" {
 }
 
 resource "digitalocean_droplet" "reverseproxy" {
-  count = "${var.enabled}"
-  image = "${var.reverseproxy_image}"
-  name = "reverseproxy.${var.environment}"
-  region = "${var.do_region}"
-  size = "s-1vcpu-1gb"
+  count              = var.enabled
+  image              = var.reverseproxy_image
+  name               = "reverseproxy.${var.environment}"
+  region             = var.do_region
+  size               = "s-1vcpu-1gb"
   private_networking = true
-  monitoring = true
-  tags = ["${digitalocean_tag.reverseproxy.name}"]
-  ssh_keys = ["${var.ssh_key}"]
-  user_data = "${data.template_file.userdata.rendered}"
+  monitoring         = true
+  tags               = [digitalocean_tag.reverseproxy.name]
+  ssh_keys           = [var.ssh_key]
+  user_data          = data.template_file.userdata.rendered
 }
 
 resource "digitalocean_floating_ip" "reverseproxy" {
-  droplet_id = "${digitalocean_droplet.reverseproxy.id}"
-  region     = "${digitalocean_droplet.reverseproxy.region}"
+  droplet_id = digitalocean_droplet.reverseproxy.id
+  region     = digitalocean_droplet.reverseproxy.region
 }
