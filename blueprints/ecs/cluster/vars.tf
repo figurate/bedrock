61bd48e648f07ed7a662f4bf595c8d1d93190d74
Ben Fortuna <bfortuna@odecee.com.au>
2019-07-12 11:44:57 +1000

    Added ecs service codedeploy

--- blueprints/ecs/cluster/vars.tf
+++ blueprints/ecs/cluster/vars.tf
@@ -3,6 +3,15 @@ variable "cloudformation_path" {
   default     = "cloudformation"
 }
 
+variable "environment" {
+  description = "Environment categorisation used in the naming of the ECS cluster"
+}
+
+variable "context" {
+  description = "Contextual categorisation used in the naming of the ECS cluster"
+  default     = ""
+}
+
 variable "vpc_default" {
   description = "Boolean value to indicate whether the matched VPC should be default for the region"
   default     = "true"
@@ -14,15 +23,12 @@ variable "vpc_tags" {
   default     = {}
 }
 
-variable "cluster_name" {
-  description = "Name of the ECS cluster"
-}
-
 variable "hosted_zone" {
   description = "A Route53 hosted zone to associate the ECS cluster endpoint"
 }
 
 locals {
-  env_string   = ""
-  account_hash = "${substr(sha256(data.aws_caller_identity.current.account_id), -10, 10)}"
+  env_string   = var.context == "" ? var.environment : format("%s-%s", var.environment, var.context)
+  cluster_name = var.context == "" ? var.environment : format("%s-cluster-%s", var.environment, var.context)
+  account_hash = substr(sha256(data.aws_caller_identity.current.account_id), -10, 10)
 }
