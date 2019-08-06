dd325502b151bac885d0a56148bc1c71ec6c4b09
Ben Fortuna <bfortuna@odecee.com.au>
2019-08-07 09:48:24 +1000

    Refactoring bastion blueprints

--- blueprints/bastion/do/main.tf
+++ blueprints/bastion/do/main.tf
@@ -4,47 +4,45 @@
  * Provision a droplet with SSH ingress authenticated with the specified public key.
  */
 data "template_file" "userdata" {
-  template = "${file(format("%s/%s.yml", var.userdata_path, var.image_os))}"
+  template = file(format("%s/%s.yml", var.userdata_path, var.image_os))
 
-  vars {
-    AuthorizedUserName   = "${var.bastion_user}"
-    AuthorizedUserSSHKey = "${var.ssh_key}"
-    ShutdownDelay        = "${var.shutdown_delay}"
+  vars = {
+    AuthorizedUserName   = var.bastion_user
+    AuthorizedUserSSHKey = var.ssh_key
+    ShutdownDelay        = var.shutdown_delay
   }
 }
 
 resource "digitalocean_tag" "bastion" {
-  name = "${local.uuid}"
+  name = local.uuid
 }
 
 resource "digitalocean_droplet" "bastion" {
-  count              = "${var.enabled}"
-  image              = "${var.bastion_image}"
-  name               = "${local.uuid}"
-  region             = "${var.do_region}"
+  count              = var.enabled == "true" ? 1 : 0
+  image              = var.bastion_image
+  name               = "${local.uuid}.${var.apex_domain}"
+  region             = var.do_region
   size               = "s-1vcpu-1gb"
   private_networking = true
   monitoring         = true
-  tags               = ["${digitalocean_tag.bastion.name}"]
-  ssh_keys           = ["${var.ssh_key}"]
-  user_data          = "${data.template_file.userdata.rendered}"
+  tags               = [digitalocean_tag.bastion.name]
+  //  ssh_keys           = [var.ssh_key]
+  user_data = data.template_file.userdata.rendered
 }
 
 resource "null_resource" "bastion_ssh_key" {
+  count      = var.enabled == "true" ? 1 : 0
   depends_on = ["digitalocean_droplet.bastion"]
-
   provisioner "local-exec" {
-    command = "scp ${var.ssh_private_key} root@${digitalocean_droplet.bastion.ipv4_address}:/root/.ssh"
+    command = "scp -o StrictHostKeyChecking=no ${var.ssh_private_key} ${var.bastion_user}@${digitalocean_droplet.bastion[0].ipv4_address}:.ssh"
   }
-
   provisioner "remote-exec" {
-    inline = ["chmod 600 /root/.ssh/id_rsa"]
-
+    inline = ["chmod 600 /home/${var.bastion_user}/.ssh/id_rsa"]
     connection {
       type        = "ssh"
-      user        = "root"
-      host        = "${digitalocean_droplet.bastion.ipv4_address}"
-      private_key = "${file(var.ssh_private_key)}"
+      user        = var.bastion_user
+      host        = digitalocean_droplet.bastion[0].ipv4_address
+      private_key = file(var.ssh_private_key)
     }
   }
 }
