c2f178e02c017bd84cf7470281b1edeaa58b0331
Ben Fortuna <bfortuna@odecee.com.au>
2019-10-14 11:14:35 +1100

    Updated to support multiple instances

--- blueprints/nginx/reverseproxy/do/main.tf
+++ blueprints/nginx/reverseproxy/do/main.tf
@@ -3,13 +3,17 @@
  *
  * Provision a droplet with NGINX and letsencrypt installed.
  */
+data digitalocean_ssh_key ssh_key {
+  name = var.ssh_key
+}
+
 data "template_file" "userdata" {
-  template = file(format("%s/%s.yml", var.userdata_path, var.image_os))
+  template = file(format("%s/%s.yml", var.template_path, var.image_os))
   vars = {
     NginxAmplifyKey      = var.amplify_key
     NginxHostname        = "${var.environment}-reverseproxy"
-    AuthorizedUserName   = var.reverseproxy_user
-    AuthorizedUserSSHKey = file(var.ssh_key)
+    AuthorizedUserName   = var.ssh_user
+    AuthorizedUserSSHKey = data.digitalocean_ssh_key.ssh_key.public_key
     PapertrailHost       = var.papertrail_host
     PapertrailPort       = var.papertrail_port
   }
@@ -20,19 +24,20 @@ resource "digitalocean_tag" "reverseproxy" {
 }
 
 resource "digitalocean_droplet" "reverseproxy" {
-  count              = var.enabled
-  image              = var.reverseproxy_image
-  name               = "reverseproxy.${var.environment}"
+  count              = var.instance_count
+  image              = var.droplet_image
+  name               = "${var.environment}${count.index + 1}-reverseproxy.${var.apex_domain}"
   region             = var.do_region
   size               = "s-1vcpu-1gb"
   private_networking = true
   monitoring         = true
   tags               = [digitalocean_tag.reverseproxy.name]
-  ssh_keys           = [var.ssh_key]
+  ssh_keys           = [data.digitalocean_ssh_key.ssh_key.fingerprint]
   user_data          = data.template_file.userdata.rendered
 }
 
-resource "digitalocean_floating_ip" "reverseproxy" {
-  droplet_id = digitalocean_droplet.reverseproxy.id
-  region     = digitalocean_droplet.reverseproxy.region
+resource "digitalocean_floating_ip_assignment" "reverseproxy" {
+  count      = length(var.floatingip_addresses)
+  droplet_id = digitalocean_droplet.reverseproxy[count.index].id
+  ip_address = var.floatingip_addresses[count.index]
 }
