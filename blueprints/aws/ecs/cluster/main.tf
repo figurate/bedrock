08f771d4f7ee32bc8c88c69b3bde12e9b367e972
Ben Fortuna <bfortuna@odecee.com.au>
2019-10-16 12:24:52 +1100

    Added ssl certificate

--- blueprints/aws/ecs/cluster/main.tf
+++ blueprints/aws/ecs/cluster/main.tf
@@ -23,6 +23,31 @@ data "aws_subnet_ids" "tenant" {
   //  }
 }
 
+resource "tls_private_key" "ssl_certificate" {
+  algorithm = "RSA"
+}
+
+resource "tls_self_signed_cert" "ssl_certificate" {
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+  key_algorithm         = "RSA"
+  private_key_pem       = tls_private_key.ssl_certificate.private_key_pem
+  validity_period_hours = 2160 // 6 months
+  subject {
+    common_name  = format("%s.service.internal", local.cluster_name)
+    organization = ""
+    country      = ""
+  }
+}
+
+resource "aws_acm_certificate" "ssl_certificate" {
+  private_key      = tls_private_key.ssl_certificate.private_key_pem
+  certificate_body = tls_self_signed_cert.ssl_certificate.cert_pem
+}
+
 resource "aws_cloudformation_stack" "ecs_cluster" {
   name         = local.cluster_name
   capabilities = ["CAPABILITY_IAM"]
@@ -35,7 +60,7 @@ resource "aws_cloudformation_stack" "ecs_cluster" {
     TargetGroupName           = local.cluster_name
     TargetDeregistrationDelay = ""
     HealthCheckPath           = "/"
-    CertificateArn            = ""
+    CertificateArn            = aws_acm_certificate.ssl_certificate.arn
     ClusterName               = local.cluster_name
     HostedZoneId              = data.aws_route53_zone.tenant.id
     RouteName                 = "${local.cluster_name}-${local.account_hash}"
