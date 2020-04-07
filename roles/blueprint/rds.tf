e3e50bee742be546d69900f86b30505060792fc1
Ben Fortuna <bfortuna@odecee.com.au>
2020-04-07 11:58:38 +1000

    Consolidate iam policies

---
+++ roles/blueprint/rds.tf
@@ -0,0 +1,21 @@
+data "aws_iam_policy_document" "rds_admin" {
+  statement {
+    actions = [
+      "rds:StopDBInstance",
+      "rds:StartDBInstance",
+      "rds:StopDBCluster",
+      "rds:StartDBCluster",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+      "cloudwatch:Describe*",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "rds_admin" {
+  name   = "bedrock-rds-admin"
+  policy = data.aws_iam_policy_document.rds_admin.json
+}
