df4f47d409b133bfc9d205088fb6caa4a808baed
Ben Fortuna <bfortuna@odecee.com.au>
2020-04-09 14:36:49 +1000

    Support app mesh and cluster without a load balancer

---
+++ blueprints/aws/ecs/cluster/ssl.tf
@@ -0,0 +1,27 @@
+/*
+ * Provision a self-signed certificate and import to AWS ACM.
+ */
+resource "tls_private_key" "ssl_certificate" {
+  algorithm = "RSA"
+}
+
+resource "tls_self_signed_cert" "ssl_certificate" {
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+  key_algorithm         = "RSA"
+  private_key_pem       = tls_private_key.ssl_certificate.private_key_pem
+  validity_period_hours = 2160 // 6 months
+  subject {
+    common_name  = format("%s.%s", local.cluster_name, var.zone_name)
+    organization = ""
+    country      = ""
+  }
+}
+
+resource "aws_acm_certificate" "ssl_certificate" {
+  private_key      = tls_private_key.ssl_certificate.private_key_pem
+  certificate_body = tls_self_signed_cert.ssl_certificate.cert_pem
+}
--- blueprints/aws/ecs/cluster/vars.tf
+++ blueprints/aws/ecs/cluster/vars.tf
@@ -3,14 +3,23 @@ variable "cloudformation_path" {
   default     = "cloudformation"
 }
 
+variable "cluster_template" {
+  description = "The CloudFormation template used to create the ECS cluster"
+  default     = "ecs_cluster"
+}
+
+variable "environment" {
+  description = "The name of the environment associated with the cluster"
+}
+
 variable "context" {
-  description = "Contextual categorisation used in the naming of the ECS cluster"
+  description = "Contextual naming of the ECS cluster (eg. user, service, etc.)"
   default     = ""
 }
 
 variable "vpc_default" {
-  description = "Boolean value to indicate whether the matched VPC should be default for the region"
-  default     = "true"
+  description = "Indicate whether to deploy in the default VPC"
+  default     = true
 }
 
 variable "vpc_tags" {
@@ -19,12 +28,30 @@ variable "vpc_tags" {
   default     = {}
 }
 
-variable "hosted_zone" {
+variable "zone_name" {
   description = "A Route53 hosted zone to associate the ECS cluster endpoint"
+}
+
+variable "private_routing" {
+  description = "Indicates whether private routing (zone) is enabled"
+  default     = "True"
+  //  validation {
+  //    condition = var.private_routing == "true" || var.private_routing == "false"
+  //    error_message = "Must be a boolean value"
+  //  }
+}
+
+variable "appmesh_enabled" {
+  description = "Enable AppMesh for the cluster"
+  default     = "False"
+  //  validation {
+  //    condition = var.appmesh_enabled == "true" || var.appmesh_enabled == "false"
+  //    error_message = "Must be a boolean value"
+  //  }
 }
 
 locals {
   env_string   = var.context == "" ? var.environment : format("%s-%s", var.environment, var.context)
-  cluster_name = var.context == "" ? var.environment : format("%s-cluster-%s", var.environment, var.context)
+  cluster_name = var.context == "" ? format("%s-cluster", var.environment) : format("%s-cluster-%s", var.environment, var.context)
   account_hash = substr(sha256(data.aws_caller_identity.current.account_id), -10, 10)
 }
