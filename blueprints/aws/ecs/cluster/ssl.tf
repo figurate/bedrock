c1a6a8e395daea541b70c41e39f35d7a249250bb
Ben Fortuna <bfortuna@odecee.com.au>
2020-04-21 21:25:16 +1000

    Clean up

--- blueprints/aws/ecs/cluster/ssl.tf
+++ blueprints/aws/ecs/cluster/ssl.tf
@@ -15,7 +15,7 @@ resource "tls_self_signed_cert" "ssl_certificate" {
   private_key_pem       = tls_private_key.ssl_certificate.private_key_pem
   validity_period_hours = 2160 // 6 months
   subject {
-    common_name  = format("%s.%s", local.cluster_name, var.zone_name)
+    common_name  = format("*.%s", local.default_namespace)
     organization = ""
     country      = ""
   }
