c8de476ad1ecc1e90de235ef97ef8407d4f5ddbd
Ben Fortuna <bfortuna@odecee.com.au>
2021-01-21 17:46:22 +1100

    Updated docker provider

--- blueprints/aws/ecr-repository/main.tf
+++ blueprints/aws/ecr-repository/main.tf
@@ -1,7 +1,7 @@
 module "repository" {
   source = "figurate/ecr-repository/aws"
 
-  name                       = "nginx"
+  name                       = var.name
   scan_on_push               = var.scan_on_push
   untagged_image_expiry_days = var.untagged_image_expiry_days
 }
@@ -10,8 +10,8 @@ module "import" {
   count  = var.import_enabled ? length(var.source_tags) : 0
   source = "figurate/docker-container/docker//modules/ecr"
 
-  name       = "nginx_push_${var.source_tags[count.index]}"
-  command    = ["push", "nginx", var.source_tags[count.index], var.source_tags[count.index]]
+  name       = "${var.name}_push_${var.source_tags[count.index]}"
+  command    = ["push", var.name, var.source_tags[count.index], var.source_tags[count.index]]
   depends_on = [module.repository]
   rm         = true
   aws_region = var.aws_region
