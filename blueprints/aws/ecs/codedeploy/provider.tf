b24b35fa707c04558c771d96b2a5aa70b93c4a3c
Ben Fortuna <bfortuna@odecee.com.au>
2021-03-20 13:27:07 +1100

    Refactoring

--- blueprints/aws/ecs/codedeploy/provider.tf
+++
@@ -1,9 +0,0 @@
-provider aws {
-  assume_role {
-    role_arn = "arn:aws:iam::${var.assume_role_account}:role/bedrock-ecs-service-admin"
-  }
-}
-
-variable assume_role_account {
-  description = "AWS account ID for the role to assume into"
-}
