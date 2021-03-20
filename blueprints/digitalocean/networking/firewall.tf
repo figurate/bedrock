fe895aaf7daba5f74afecdd0d19c896d240cf3a0
Ben Fortuna <bfortuna@odecee.com.au>
2021-03-20 15:30:34 +1100

    Added support for digital ocean firewalls

---
+++ blueprints/digitalocean/networking/firewall.tf
@@ -0,0 +1,8 @@
+module "firewall_egress" {
+  source = "figurate/firewall/digitalocean"
+  count = length(var.firewalls)
+
+  name = var.firewalls[count.index][0]
+  ingress_rules = var.firewalls[count.index][1]
+  egress_rules = var.firewalls[count.index][2]
+}
